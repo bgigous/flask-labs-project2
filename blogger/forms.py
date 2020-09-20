0e6c450882d4ff51e14aee5daec3f390cfa6fdfe
gigous <bgigous@protonmail.com>
2020-09-19 22:50:48 -0600

    Get project into working state, py2 -> py3 Remove trailing whitespace

--- blogger/forms.py
+++ blogger/forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import TextField, TextAreaField, SubmitField, PasswordField, BooleanField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Length, Email
@@ -6,13 +6,13 @@ from wtforms.validators import DataRequired, Length, Email
 def length_check(form,field):
     if len(field.data) == 0:
         raise ValidationError('Fields should not be null')
-    
 
-class AddPostForm(Form):
+
+class AddPostForm(FlaskForm):
     title = TextField('Title', validators=[ DataRequired()])
     description = TextAreaField('Description', validators = [DataRequired()])
 
-class SignUpForm(Form):
+class SignUpForm(FlaskForm):
     firstname= TextField('First Name', validators= [DataRequired(), length_check])
     lastname = TextField('Last Name', validators= [DataRequired()])
     username = TextField('User Name', validators= [ DataRequired(), Length(min=4)])
@@ -21,13 +21,13 @@ class SignUpForm(Form):
     submit = SubmitField('Sign Up')
 
 
-class SignInForm(Form):
+class SignInForm(FlaskForm):
     email = EmailField('Email', validators = [DataRequired(), Email()])
     password = PasswordField('Password', validators = [DataRequired(), Length(min=6, max=30)])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Sign In')
 
-class AboutUserForm(Form):
+class AboutUserForm(FlaskForm):
     firstname= TextField('First Name', validators= [DataRequired(), length_check])
     lastname = TextField('Last Name', validators= [DataRequired()])
     username = TextField('User Name', validators= [ DataRequired(), Length(min=4)])
